7f2154fb41727fe52b3d4d0f5bb2287fa1316371
Zach Cullen <zacullen@gmail.com>
2020-05-20 11:16:04 -0500

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,27 @@
-// Code ClickityClick Component Here
+import React from 'react'
+
+export default class ClickityClick extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      toggle: false,
+    }
+  }
+
+  handleClick = () => {
+    this.setState(previousState => {
+      return { toggle: !previousState.toggle }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This light is {this.state.toggle ? 'ON' : 'OFF'}</p>
+        <button onClick={this.handleClick}>
+          {this.state.toggle ? 'TURN OFF' : 'TURN ON'}
+        </button>
+      </div>
+    )
+  }
+}
